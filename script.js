ff61dec78e09eb2c8ed8a856a7ca972cb597a939
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2024-08-05 17:38:25 -0300

    Changing background text

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+//Variáveis
 const html = document.querySelector('html')
 const botao_foco = document.querySelector('.app__card-button--foco')
 const botao_curto = document.querySelector('.app__card-button--curto')
 const botao_longo = document.querySelector('.app__card-button--longo')
 const imagens = document.querySelector('.app__image')
+const altera_frases = document.querySelector('.app__title')
 
 //Funções ao clicar nos botões
 botao_foco.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'foco')
-    imagens.setAttribute('src', '/imagens/foco.png')
+    alterarContexto('foco')
 })
 
 botao_curto.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-curto')
-    imagens.setAttribute('src', '/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
 })
 
 botao_longo.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-longo')
-    imagens.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 })
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto);
+    imagens.setAttribute('src', `/imagens/${contexto}.png`);
+    switch(contexto){
+        case "foco":
+            altera_frases.innerHTML = `
+            Otimize sua produtividade, <br>
+                <strong class="app__title-strong">mergulhe no que importa</strong>
+            `
+            break;
+        case "descanso-curto":
+            altera_frases.innerHTML =`
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":
+            altera_frases.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+            break;
+    }
+}
