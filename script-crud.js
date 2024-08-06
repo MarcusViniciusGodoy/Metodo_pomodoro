ede54e8fd2adf974306808c6869d165db3876903
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2024-08-06 17:17:35 -0300

    Adding tasks

--- script-crud.js
+++
@@ -1,6 +0,0 @@
-const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
-const formAdicionartarefa = document.querySelector('.app__form-add-task');
-
-btnAdicionarTarefa.addEventListener('click', () => {
-    formAdicionartarefa.classList.toggle('hidden');
-})
